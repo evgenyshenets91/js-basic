3a6727e9808b40006941edb0ba94c0b75ce9f0a6
Evgeny <evgenyshenets91@gmail.com>
2019-01-14 23:33:43 +0300

    Калькулятор при помощи функции-конструктора

---
+++ constructor-function-calculator.js
@@ -0,0 +1,19 @@
+
+function Calculator() {
+
+    this.read = function() {
+        this.a = +prompt('Введите a?', 0);
+        this.b = +prompt('Введите b?', 0);
+    };
+    this.sum = function() {
+        return this.a + this.b;
+    };
+    this.mul = function() {
+        return this.a * this.b;
+    };
+}
+var calculator = new Calculator();
+calculator.read();
+
+alert( "Сумма=" + calculator.sum() );
+alert( "Произведение=" + calculator.mul() );
